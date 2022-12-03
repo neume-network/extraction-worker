59685855686d0f77c1e48a6d30f251799aaed374
il3ven <vaibhavchanana@yahoo.co.in>
2022-12-03 16:03:13 +0530

    add async-retry to retry HTTP requests

--- src/request.mjs
+++ src/request.mjs
@@ -1,41 +1,56 @@
 import fetch from "cross-fetch";
+import retry from "async-retry";
 
 export async function request(url, method, body, headers, signal) {
-  let options = {
-    method,
-  };
+  return retry(
+    async (bail) => {
+      let options = {
+        method,
+      };
 
-  if (body) {
-    options.body = body;
-  }
-  if (headers) {
-    options.headers = headers;
-  }
-  if (signal) {
-    options.signal = signal;
-  }
+      if (body) {
+        options.body = body;
+      }
+      if (headers) {
+        options.headers = headers;
+      }
+      if (signal) {
+        options.signal = signal;
+      }
 
-  // NOTE: We let `fetch` throw. Error must be caught on `request` user level.
-  const results = await fetch(url, options);
-  const answer = await results.text();
+      // NOTE: We let `fetch` throw. Error must be caught on `request` user level.
+      const results = await fetch(url, options);
+      const answer = await results.text();
 
-  if (results.status >= 400) {
-    throw new Error(
-      `Request to url "${url}" with method "${method}" and body "${JSON.stringify(
-        body
-      )}" unsuccessful with status: ${results.status} and answer: "${answer}"`
-    );
-  }
+      if (results.status === 403 || results.status === 404) {
+        bail(
+          new Error(
+            `Request to url "${url}" with method "${method}" and body "${JSON.stringify(
+              body
+            )}" unsuccessful with status: ${
+              results.status
+            } and answer: "${answer}"`
+          )
+        );
+      }
 
-  try {
-    return JSON.parse(answer);
-  } catch (err) {
-    if (results.headers.get("Content-Type")?.toLowerCase().includes("json")) {
-      throw new Error(
-        `Encountered error when trying to parse JSON body result: "${answer}", error: "${err.toString()}"`
-      );
-    }
+      try {
+        return JSON.parse(answer);
+      } catch (err) {
+        if (
+          results.headers.get("Content-Type")?.toLowerCase().includes("json")
+        ) {
+          bail(
+            new Error(
+              `Encountered error when trying to parse JSON body result: "${answer}", error: "${err.toString()}"`
+            )
+          );
+        }
 
-    return answer;
-  }
+        return answer;
+      }
+    },
+    // TODO: Make this configurable
+    { retries: 5 }
+  );
 }
